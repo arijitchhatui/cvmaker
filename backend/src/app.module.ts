4d05e10aacf91af835ec5bb5922b579ca6c41621
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-12-24 19:45:24 -0300

    feat: update environment configuration and API prefix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { join } from "path";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
@@ -10,6 +11,7 @@ import envGlobal from "./configs/env.global";
     ConfigModule.forRoot({
       isGlobal: true,
       load: [envGlobal],
+      envFilePath: [join(process.cwd(), "..", ".env")],
     }),
   ],
   controllers: [AppController],
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { join } from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  envDir: join(process.cwd(), ".."),
   plugins: [
     tailwindcss(),
     react({
