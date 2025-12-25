49833ffb9f5c31a5b89cdece08569d7b32943606
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-12-25 15:15:04 -0300

    feat: expand CreateCvDto with additional fields and nested contacts structure

--- backend/src/cvs-module/dto/create-cv.dto.ts
+++ backend/src/cvs-module/dto/create-cv.dto.ts
@@ -1,26 +1,117 @@
-import { IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import {
+  IsArray,
+  IsNumber,
+  IsObject,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from "class-validator";
+
+class CreateCvContactsDto {
+  @ApiProperty({
+    type: String,
+    nullable: true,
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  public email: string | null;
+
+  @ApiProperty({
+    type: String,
+    nullable: true,
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  public phone: string | null;
+}
 
 export class CreateCvDto {
+  @ApiProperty()
   @IsString()
   public templateId: string;
 
+  @ApiProperty()
+  @IsString()
   public id: string;
+
+  @ApiProperty()
+  @IsString()
   public cVName: string;
 
+  @ApiProperty()
+  @IsString()
   public firstName: string;
+
+  @ApiProperty({
+    type: String,
+    nullable: true,
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
   public middleName: string | null;
+
+  @ApiProperty()
+  @IsString()
   public lastName: string;
+
+  @ApiProperty({
+    type: String,
+    nullable: true,
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
   public nickname: string | null;
+
+  @ApiProperty({
+    type: String,
+    nullable: true,
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
   public avatar: string | null;
 
-  public contacts: {
-    email: string | null;
-    phone: string | null;
-  };
+  @ApiProperty()
+  @ValidateNested()
+  @Type(() => CreateCvContactsDto)
+  @IsObject()
+  public contacts: CreateCvContactsDto;
 
+  @ApiProperty({
+    type: String,
+    nullable: true,
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
   public address: string | null;
+
+  @ApiProperty({
+    type: String,
+    nullable: true,
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
   public summary: string | null;
+
+  @ApiProperty({
+    type: String,
+    nullable: true,
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
   public objectives: string | null;
+
+  @ApiProperty({ type: [Object] })
+  @IsArray()
   public education: Array<{
     institution: string;
     degree: string;
@@ -31,6 +122,7 @@ export class CreateCvDto {
     description: string | null;
     location: string | null;
   }>;
+
   public experience: Array<{
     company: string;
     position: string;
@@ -39,10 +131,12 @@ export class CreateCvDto {
     responsibilities: string | null;
     location: string | null;
   }>;
+
   public skills: Array<{
     name: string;
     level: string | null;
   }>;
+
   public projects: Array<{
     name: string;
     description: string;
@@ -51,6 +145,7 @@ export class CreateCvDto {
     link: string | null;
     location: string | null;
   }>;
+
   public certifications: Array<{
     name: string;
     issuingOrganization: string;
@@ -59,14 +154,25 @@ export class CreateCvDto {
     credentialID: string | null;
     credentialURL: string | null;
   }>;
+
   public languages: Array<{
     name: string;
     proficiency: string;
   }>;
+
   public hobbies: Array<{
     description: string;
   }>;
+
+  @ApiProperty({
+    type: String,
+    nullable: true,
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
   public additionalInfo: string | null;
+
   public otherExperiences: Array<{
     title: string;
     description: string;
@@ -74,16 +180,27 @@ export class CreateCvDto {
     endDate: string | null;
     location: string | null;
   }>;
+
   public references: Array<{
     name: string;
     relationship: string | null;
     contactInfo: string | null;
   }>;
+
   public links: Array<{
     label: string;
     url: string;
   }>;
 
+  @ApiProperty({
+    example: Date.now(),
+  })
+  @IsNumber()
   public createdAt: number;
+
+  @ApiProperty({
+    example: Date.now(),
+  })
+  @IsNumber()
   public updatedAt: number;
 }
