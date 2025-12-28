851726d8eebebdfb59122f973ce76a3e21fe8a5c
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-12-28 17:58:06 -0300

    fix: remove server timeout configuration from bootstrap function feat: add timeout configuration to axios instance for API requests

--- frontend/src/lib/apiInstance.ts
+++ frontend/src/lib/apiInstance.ts
@@ -4,6 +4,7 @@ const nodeEnv = import.meta.env.DEV;
 
 const apiInstance = axios.create({
   baseURL: nodeEnv ? "http://localhost:3000/api" : "/api",
+  timeout: 60000, // 60 seconds
 });
 
 export default apiInstance;
